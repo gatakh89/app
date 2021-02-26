152edd9499a6d80aa3d7c52062a36b762e0d5ed6
gatakh89 <gatakh89@gmail.com>
2021-02-26 14:04:24 +0900

    'd'

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+  useEffect(() => {
+
+    messaging().getToken()
+    .then(token => {
+      console.log("--token--");
+      console.log(token);
+    }); 
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
